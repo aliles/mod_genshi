2c256775d4fcdc6e0bc125726aeba015b24637ec
Aaron Iles <aaron.iles@gmail.com>
2012-04-22 21:27:27 +1000

    Development server can run on any port.
    Port supplied as first argument to command line, similar to
    SimpleHTTPServer.

--- mod_genshi/server.py
+++ mod_genshi/server.py
@@ -1,9 +1,13 @@
 """mod_genshi development server.
 """
 from wsgiref.simple_server import make_server
+import sys
 
 from mod_genshi.app import handler
 
 if __name__ == '__main__':
-    httpd = make_server('', 8000, handler)
+    port = 8000
+    if len(sys.argv) > 1:
+        port = int(sys.argv[1])
+    httpd = make_server('', port, handler)
     httpd.serve_forever()
